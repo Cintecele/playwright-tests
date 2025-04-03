09bde13bd92baa464e526fe2a8328fa669da2129
d.lebedev <d.lebedev@infinnity.ru>
2025-04-03 18:44:35 +0500

    wip

--- testConfig.ts
+++ testConfig.ts
@@ -2,7 +2,7 @@ export const testConfig = {
  //   qa: `https://demoqa.com`,
     qa:'https://dev114.reglab.ru/arm-engineer/index.html#/main/projects',
     dev: ``,
-    qaApi: `https://reqres.in`,
+    qaApi: `https://dev114.reglab.ru/`,
     devApi: ``,
     username: `demouat@gmail.com`,
     password: `U2FsdGVkX18/eMdsOJpvI4hJZ/w7hNgwSRFaDvAcZx4=`,
--- tests/api/GET.test.ts
+++ tests/api/GET.test.ts
@@ -1,19 +1,83 @@
-import { APIActions } from '@lib/APIActions';
-import { test } from '@playwright/test';
+import {HttpHeaders} from '@angular/common/http';
 
-const apiActions = new APIActions();
+import {expect, test} from "@playwright/test";
 
-test(`getUsers`, { tag: '@API'}, async ({ request }) => {
-    const response = await request.get(`/api/users?per_page=1`);
-    await apiActions.verifyStatusCode(response);
 
-    //* Body Response Params and Body Response Headers are stored in single text file separated by #
-    const responseBodyParams = (await apiActions.readValuesFromTextFile(`getUsers`)).split(`#`)[0];
-    await apiActions.verifyResponseBody(responseBodyParams, await response.json(), `Response Body`);
+// test(`About`, {tag: '@API'}, async ({request}) => {
+//     const response = await request.get(`summit/api/About`);
+//     await apiActions.verifyStatusCode(response);
+//
+//     //* Body Response Params and Body Response Headers are stored in single text file separated by #
+//     const responseBodyParams = (await apiActions.readValuesFromTextFile(`getUsers`)).split(`#`)[0];
+//     await apiActions.verifyResponseBody(responseBodyParams, await response.json(), `Response Body`);
+//
+//     const responseBodyHeaders = (await apiActions.readValuesFromTextFile(`getUsers`)).split(`#`)[1];
+//     await apiActions.verifyResponseHeader(responseBodyHeaders, response.headersArray(), `Response Headers`);
+// });
 
-    const responseBodyHeaders = (await apiActions.readValuesFromTextFile(`getUsers`)).split(`#`)[1];
-    await apiActions.verifyResponseHeader(responseBodyHeaders, response.headersArray(), `Response Headers`);
+test(`Get_About`, {tag: '@API'}, async ({request}) => {
+    const response = await request.get(`https://dev114.reglab.ru/summit/api/About`);
+    expect(response.status()).toBe(200);
+    console.log(`Статус код: ${response.status()}`);
+    console.log('Ответ: ', response.headersArray());
+    console.log('Тело ответа', response.text())
 });
+
+test(`POST_CREATE_PROJECT`, {tag: '@API'}, async ({request}) => {
+  //  let url_ = 'https://dev114.reglab.ru/summit' + "/api/Project";
+
+    const name = 'yuyuyuy';
+    const content_ = new FormData();
+    if (name !== null && name !== undefined)
+        content_.append("name", name.toString());
+    // if (linkedPackages !== null && linkedPackages !== undefined)
+    //     linkedPackages.forEach(item_ => content_.append("linkedPackages", item_.toString()));
+
+    let options_: any = {
+        body: content_,
+        observe: "response",
+        responseType: "blob",
+        headers: new HttpHeaders({
+            "Accept": "application/json"
+        })
+    };
+    const response = await request.post('https://dev114.reglab.ru/summit/api/Project', options_);
+    expect(response.status()).toBe(200);
+});
+
+//     const form = new FormData();
+//     form.append('name', new Date().getTime().toString());
+//     form.append('linkedPackages', '0195f4e8-410b-78f2-b1b2-86383c0838a6');
+//     console.log(form);
+//     // // Получаем заголовки из формы, чтобы использовать правильный boundary
+//     // const headers = form.getHeaders();
+//     //
+//     // // Добавляем другие заголовки
+//     // headers['Host'] = 'dev114.reglab.ru';
+//     // headers['Content-Length'] = form.getLengthSync().toString();
+//
+//  //   const size = form.getLengthSync().toString();
+//     const headers = {
+//         "Accept": "application/json",
+//         "Content-Type": "multipart/form-data; boundary=----WebKitFormBoundarytm6isrvP32YxXkud",
+//         "Host": "dev114.reglab.ru",
+//         "Content-Length": "318"
+//     };
+//
+//     let options_ : any = {
+//         headers,
+//         body: form
+//     };
+//
+//     console.log('Параметры запроса: ', options_);
+//     // Отправляем запрос с multipart/form-data
+//     const response = await request.post('https://dev114.reglab.ru/summit/api/Project', options_);
+//  await expect(response.status()).toBe(200);
+//
+//    console.log(`Статус код: ${response.status()}`);
+//     console.log('Ответ: ', response.headersArray());
+//     console.log('Тело ответа', response.text())
+// });
 
 //* In Case you application has token system, Please use the below code
 
