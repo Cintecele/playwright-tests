a353cdca4fc0fdf051dc8aec1b6868b73df69ab3
d.lebedev <darkvigour@gmail.com>
2025-04-08 08:15:03 +0500

    Добавить константы

--- src/main/utils/constants.ts
+++ src/main/utils/constants.ts
@@ -6,5 +6,22 @@ export class constants {
     public static readonly Headers = class {
 
         public static enable_content_packages = "Включить контент пакеты";
+        public static new_project = "Новый проект";
+        public static all_projects = "Все проекты";
+        public static authorization = "Авторизация";
     }
+
+    public static readonly Placeholders = class {
+        public static login_placeholder = "Логин";
+        public static password_placeholder = "Пароль";
+        public static content_package_or_component_placeholder = "Название пакета или компонента";
+        public static project_name_placeholder = "Название проекта";
+    }
+
+    public static readonly Buttons = class{
+        public static continue = "Продолжить";
+        public static cancel = "Отменить";
+        public static back = "Назад";
+        public static create_project = "Создать проект";
+}
 }
