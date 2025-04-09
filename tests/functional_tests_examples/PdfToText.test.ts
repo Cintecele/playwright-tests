7aae90016dc2378a8f52f5f623c047d8453fd326
d.lebedev <darkvigour@gmail.com>
2025-04-09 16:43:41 +0500

    Удалить дубль директории

--- tests/functional_tests_examples/PdfToText.test.ts
+++ tests/functional_tests_examples/PdfToText.test.ts
@@ -2,7 +2,7 @@ import test from '../../src/main/lib/BaseTest';
 import { expect } from '@playwright/test';
 
 test(`Verify the text contents of PDF file.`, { tag: '@Smoke'}, async ({ webActions }) => {
-    const extractedText = await webActions.getPDFText("./utils/functional/sample.pdf");
+    const extractedText = await webActions.getPDFText("src/main/utils/Files/sample.pdf");
     const status = extractedText.includes("A Simple PDF File")
     expect(status).toBeTruthy();
 });
